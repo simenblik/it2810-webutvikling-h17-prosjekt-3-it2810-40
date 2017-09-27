09deb1fcf1030b894fa8c3928baf6c5a012c66c1
matssa <matssa@stud.ntnu.no>
2017-09-27 14:21:25 +0200

    added something

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Card from './components/common/Card.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-          <Header/>
-
+        {' '}
+        <div>
+          <Header />{' '}
+        </div>
+        <div className="cardStyle">
+          <Card />
+        </div>
       </div>
     );
   }
--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -2,24 +2,25 @@ import React from 'react';
 
 //er en container som kan ha
 
-const Card = (props) => {
-    const {} = styles;
-
-    return (
-        <div>
-            {props.children}
-        </div>
-    )
-
+const Card = props => {
+  const { buttonStyle, cardStyle } = styles;
+  return (
+    <div>
+      <p>stuff</p>
+    </div>
+  );
+  // return <div>{props.children}</div>;
 };
-
 
 const styles = {
-    buttonStyle: {
-        flex: 1
-
-    }
-
+  buttonStyle: {
+    flex: 1,
+  },
+  cardStyle: {
+    flex: 1,
+    marginTop: 20,
+    padding: 5,
+  },
 };
 
-export {Card}
+export default Card;
