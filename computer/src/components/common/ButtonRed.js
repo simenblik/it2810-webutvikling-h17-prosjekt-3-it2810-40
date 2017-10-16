f35efe24fe4ae2deff5267eeaed5c15cfdf28a55
matssa <matssa@stud.ntnu.no>
2017-10-17 01:11:42 +0200

    red button now gets color as prop

--- computer/src/components/common/ButtonRed.js
+++ computer/src/components/common/ButtonRed.js
@@ -1,34 +1,33 @@
-import React from 'react';
+import React from "react";
 
-const ButtonRed = ({ onPress, children }) => {
-  const { buttonRedStyle, textStyle } = styles;
+const ButtonRed = ({ onPress, children, buttonColor }) => {
+	console.log(buttonColor);
 
-  return (
-    <button onClick={onPress} style={buttonRedStyle}>
-      <p style={textStyle}>{children}</p>
-    </button>
-  );
-};
+	const { buttonRedStyle, textStyle } = {
+		textStyle: {
+			alignSelf: "center",
+			color: "black",
+			fontSize: "15px",
+			fontWeight: "600"
+		},
 
-const styles = {
-  textStyle: {
-    alignSelf: 'center',
-    color: 'black',
-    fontSize: '15px',
-    fontWeight: '600',
-  },
-
-  buttonRedStyle: {
-    flex: '1',
-    alignSelf: 'stretch',
-    borderRadius: '5px',
-    borderWidth: '3px',
-    borderColor: 'red',
-    marginLeft: '5px',
-    marginRight: '5px',
-    backgroundColor: 'white',
-    cursor: 'pointer',
-  },
+		buttonRedStyle: {
+			flex: "1",
+			alignSelf: "stretch",
+			borderRadius: "5px",
+			borderWidth: "3px",
+			borderColor: buttonColor,
+			marginLeft: "5px",
+			marginRight: "5px",
+			backgroundColor: "white",
+			cursor: "pointer"
+		}
+	};
+	return (
+		<button onClick={onPress} style={buttonRedStyle}>
+			<p style={textStyle}>{children}</p>
+		</button>
+	);
 };
 
 export { ButtonRed };
