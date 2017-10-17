1ba3fb5de8952ec607b98a3451c1c25558d63a51
Jonas Thorsen <jonaseth@stud.ntnu.no>
2017-10-17 23:45:00 +0200

    Changed addModalScreen and added components

--- mobile/app/components/common/index.js
+++ mobile/app/components/common/index.js
@@ -1,7 +1,11 @@
-export * from './Input';
-export * from './Card';
-export * from './CardSection';
+export * from "./Input";
+export * from "./Card";
+export * from "./CardSection";
 
-export * from './Button';
-export * from './LargInput';
-export * from './RoundButton'
+export * from "./Button";
+export * from "./LargInput";
+export * from "./RoundButton";
+export * from "./Form";
+export * from "./FormSection";
+export * from "./CustomDatePicker";
+export * from "./CustomTimePicker";
