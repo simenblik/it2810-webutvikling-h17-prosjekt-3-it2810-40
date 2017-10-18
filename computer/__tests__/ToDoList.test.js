3db827304f06842dc835ca15cbd78c393989c873
Simen Blikeng <simenblikeng@gmail.com>
2017-10-18 23:47:02 +0200

    testing for gruppe 40

---
+++ computer/__tests__/ToDoList.test.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import { shallow, mount, render } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import ToDoList from '../src/components/ToDoList.js';
+
+
+it("sample", () => {
+	expect(true).toBe(true);
+});
