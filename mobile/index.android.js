d62bd44f4925365cff114ff40adf041167ae2273
Jonas Thorsen <jonaseth@stud.ntnu.no>
2017-10-18 16:17:02 +0200

    Added Expo

--- mobile/index.android.js
+++ mobile/index.android.js
@@ -1,6 +1,6 @@
-import { AppRegistry } from 'react-native';
+import 'expo';
+import {AppRegistry} from 'react-native';
 import App from './app/index';
 
 AppRegistry.registerComponent('mobileSimsalaPIM', () => App);
-
-
+AppRegistry.registerComponent('main', () => App);
